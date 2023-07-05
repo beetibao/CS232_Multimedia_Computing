d97d813db26a4b8bfded3a8a1fec0006af0f4feb
beetibao <20521107@gm.uit.edu.vn>
2023-07-05 20:12:20 +0700

    Change

--- CS232_Project/frontend.py
+++ CS232_Project/frontend.py
@@ -7,8 +7,8 @@ import pandas as pd
 import os
 
 st.set_page_config(
-    page_title= "COMPRESS/DECOMPRESS USING DCT & SVD",
-    page_icon = "🏕",
+    page_title= "COMPRESS/ DECOMPRESS USING DCT & SVD",
+    page_icon = "🎓",
     layout="wide",
     menu_items={
         'Get Help': 'https://www.facebook.com/beetibao',
